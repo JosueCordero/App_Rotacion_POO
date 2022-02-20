3c852ec32ac73cb13e17c16489c0dcf179491220
Josue Cordero <josuecor06@gmail.com>
2022-02-19 23:06:41 -0600

    Se agrego la clase BandaMoebius y la clase RotacionEnElMismoEJE para una mejor segmentacion del codigo

--- App_rotacion.py
+++ App_rotacion.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-class Product:
+class Aplicacion:
     #Constructor
     def __init__(self, window):
         # Initializations 
@@ -51,7 +51,7 @@ class Product:
 
         #Boton mostrar imagen rotada
 
-        ttk.Button(frame, text = 'Mostrar imagen rotada', command = self.imagen_rotar).grid(row = 9, columnspan = 2,pady = 20)
+        ttk.Button(frame, text = 'Mostrar imagen rotada', command = self.mostrarImagen).grid(row = 9, columnspan = 2,pady = 20)
 
     def selec_ejex(self):
         self.ejex = True
@@ -61,68 +61,91 @@ class Product:
         self.ejez = True
     
     #Rotacion de la imagen en 3D
-    def imagen_rotar(self):
-        #Variables
-        v_t = 40
-        v_s = 10
-        px = float(self.pointx.get())
-        py = float(self.pointy.get())
-        pz = float(self.pointz.get())
+    def mostrarImagen(self):
+        imagen = BandaMoebius(self)
+        accion = RotacionEnElMismoEJE(self,imagen)
+        accion.rotar_figura()
+        
 
-        teta = float(self.grados.get())*(np.pi/180) #Angulo en radianes
+class RotacionEnElMismoEJE:
+    def __init__(self,App,fig) -> None:
+        self.app = App
+        self.fig = fig
+
+        
+
+    def rotar_figura(self):
+        #Variables (Punto inicial y angulo(radianes))
+        
+
+        teta = float(self.app.grados.get())*(np.pi/180)
+
+        figura = self.fig #Figura a rotar
 
         #Crea el espacio en 3D
         fig = plt.figure()
         ax = plt.axes(projection="3d")
 
-        #Creacion de la banda de Mobius
-
-        t = np.linspace(0,2*np.pi,v_t)
-        w = .3
-        s = np.linspace(-w,w,v_s)
-        T,S = np.meshgrid(t,s)
-
-        #Radio y parametrizaciones
-        R = 1
-        x = px+(R+S*np.cos(T/2))*np.cos(T)
-        y = py+(R+S*np.cos(T/2))*np.sin(T)
-        z = (pz+S*np.sin(T/2))
-
-        #Matrices Rotaciones
+        #Matrices Rotacionales
         
         rotx = np.array([[1,0,0],[0,np.cos(teta),-np.sin(teta)],[0,np.sin(teta),np.cos(teta)]])
         roty = np.array([[np.cos(teta),0,np.sin(teta)],[0,1,0],[-np.sin(teta),0,np.cos(teta)]])
         rotz = np.array([[np.cos(teta),-np.sin(teta),0],[np.sin(teta),np.cos(teta),0],[0,0,1]])
 
-
         #Rotar los puntos en X
-        if(self.ejex==True):
-            for i in range(v_s):
-                for j in range(v_t):
-                    rxyz = np.array([[x[i][j]],[y[i][j]],[z[i][j]]])
-                    x[i][j], y[i][j], z[i][j] = np.dot(rotx,rxyz)
+        if(self.app.ejex==True):
+            for i in range(figura.v_s):
+                for j in range(figura.v_t):
+                    rxyz = np.array([[figura.x[i][j]],[figura.y[i][j]],[figura.z[i][j]]])
+                    figura.x[i][j], figura.y[i][j], figura.z[i][j] = np.dot(rotx,rxyz)
         #Rotar los puntos en y
-        if(self.ejey==True):
-            for i in range(v_s):
-                for j in range(v_t):
-                    rxyz = np.array([[x[i][j]],[y[i][j]],[z[i][j]]])
-                    x[i][j], y[i][j], z[i][j] = np.dot(roty,rxyz)
+        if(self.app.ejey==True):
+            for i in range(figura.v_s):
+                for j in range(figura.v_t):
+                    rxyz = np.array([[figura.x[i][j]],[figura.y[i][j]],[figura.z[i][j]]])
+                    figura.x[i][j], figura.y[i][j], figura.z[i][j] = np.dot(roty,rxyz)
         #Rotar los puntos en z
-        if(self.ejez==True):
-            for i in range(v_s):
-                for j in range(v_t):
-                    rxyz = np.array([[x[i][j]],[y[i][j]],[z[i][j]]])
-                    x[i][j], y[i][j], z[i][j] = np.dot(rotz,rxyz)
+        if(self.app.ejez==True):
+            for i in range(figura.v_s):
+                for j in range(figura.v_t):
+                    rxyz = np.array([[figura.x[i][j]],[figura.y[i][j]],[figura.z[i][j]]])
+                    figura.x[i][j], figura.y[i][j], figura.z[i][j] = np.dot(rotz,rxyz)
                 
-        ax.plot_surface(x,y,z,cmap="viridis")       
+        ax.plot_surface(figura.x,figura.y,figura.z,cmap="viridis")       
                 
 
 
         #ax.scatter3D(0,0,0, c = 'r')
         plt.show()
-    
-    
+
+
+class BandaMoebius:
+    def __init__(self,App) -> None:
+        self.app = App
+
+        px = float(self.app.pointx.get())
+        py = float(self.app.pointy.get())
+        pz = float(self.app.pointz.get())
+        
+        #Variables
+        self.v_t = 40
+        self.v_s = 10
+        
+        #Creacion de la banda de Mobius
+
+        t = np.linspace(0,2*np.pi,self.v_t)
+        w = .3
+        s = np.linspace(-w,w,self.v_s)
+        T,S = np.meshgrid(t,s)
+
+        #Radio y parametrizaciones
+        R = 1
+        self.x = px+(R+S*np.cos(T/2))*np.cos(T)
+        self.y = py+(R+S*np.cos(T/2))*np.sin(T)
+        self.z = (pz+S*np.sin(T/2))
+
+  
 if __name__ == '__main__':
     window = Tk()
-    application = Product(window)
+    application = Aplicacion(window)
     window.mainloop()
